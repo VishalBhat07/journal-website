aa69f522cfb16e1e1cfd62ed97ef07df329b5a80
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-10-13 14:17:11 +0530

    Added dotenv for environment variables

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,22 +1,21 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDuAs7bNqzYeNfd9xzNYLAw9kJnR-cnYfo",
-  authDomain: "journalwebsite-d9720.firebaseapp.com",
-  projectId: "journalwebsite-d9720",
-  storageBucket: "journalwebsite-d9720.appspot.com",
-  messagingSenderId: "659012873967",
-  appId: "1:659012873967:web:097591c634f8b3d40ef51a",
-  measurementId: "G-4PHQTFK0LG"
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Pass the app instance to getAuth
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db }; // Export auth and db
+export { auth, db };
