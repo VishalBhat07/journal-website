8a51f0d6503859b82db5d5086bd2990a899e826e
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-10-13 14:28:06 +0530

    Added backend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import './firebaseConfig.js'
+import '../../backend/firebaseConfig.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
