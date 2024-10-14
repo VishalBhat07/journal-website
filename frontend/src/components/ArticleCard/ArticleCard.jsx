e9d9dab6d950ef6aae3e9da2d2df7bfb46a68cf1
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-10-14 08:20:20 +0530

    Changed card style and added title input field to the upload popup

--- frontend/src/components/ArticleCard/ArticleCard.jsx
+++ frontend/src/components/ArticleCard/ArticleCard.jsx
@@ -1,12 +1,12 @@
-// ArticleCard.jsx
 import React from 'react';
-import './ArticleCard.css'; // Create a separate CSS file for styling
+import './ArticleCard.css'; // Ensure you have your CSS file linked
 
 const ArticleCard = ({ article }) => {
   return (
     <div className="article-card">
-      <h2 className="article-author">{article.author}</h2>
-      <p className="article-date">Uploaded on: {new Date(article.date).toLocaleDateString()}</p>
+      <h3 className="article-title">{article.title}</h3> {/* Added title */}
+      <h4 className="article-author">By : {article.author}</h4>
+      <p className="article-date">Uploaded on : {new Date(article.date).toLocaleDateString()}</p>
       <a href={article.fileURL} target="_blank" rel="noopener noreferrer" className="article-link">View Article</a>
     </div>
   );
