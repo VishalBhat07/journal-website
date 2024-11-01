5fc2f8acf5f35030eccc77bff5c5caa2dcbcb77d
SRNI-2005 <vssreenivaas2005@gmail.com>
2024-11-01 14:42:01 +0530

    fix: navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -27,8 +27,7 @@ const Navbar = ({ onSignUpClick }) => {
   };
 
   return (
-    <nav className="navbar">
-      <img src={logo} alt="navbar-logo" className="navbar-logo"/>
+          {/* <img src={logo} alt="navbar-logo" className="navbar-logo"/>
       <h1 className="navbar-title">Materials and Processing A journal from ASM India National Council (INC)</h1>
         {user ? (
           <button className="navbar-btn" onClick={handleSignOut}>
@@ -38,8 +37,28 @@ const Navbar = ({ onSignUpClick }) => {
           <button className="navbar-btn" onClick={onSignUpClick}>
             Sign Up
           </button>
+        )} */},
+    <nav id="navbar">
+      <div>
+      <img src={logo} alt="ASM logo" height="" width="200" />
+
+      </div>
+            
+      <div id="navbar-title">Materials and Processing A journal from ASM India National Council (INC)</div>
+        
+      
+      {user ? (
+          <button className="btn btn-outline-light" onClick={handleSignOut} id="button">
+            Sign Out
+          </button>
+        ) : (
+          <button className="btn btn-outline-light" onClick={onSignUpClick} id="button">
+            Sign Up
+          </button>
         )}
+
     </nav>
+
   );
 };
 
